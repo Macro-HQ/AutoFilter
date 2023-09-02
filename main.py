3435e0273223a4bb6c2a49d82ec0902591922840
wtfismyname69420 <84185962+wtfismyname69420@users.noreply.github.com>
2023-09-01 22:53:15 -0400

    changes

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import requests
 import os
 import time
 from datetime import datetime
+import re
 
 import yaml
 with open('config.yaml', 'r') as f:
@@ -45,33 +46,52 @@ os.system("tmux new-session -d -s bin './binmaster-v2-linux'")
 
 @client.event
 async def on_ready():
-    print(f"We have logged in as {str(client)}")
+    print(f"Logged in as: {str(client)}")
 
 
 @client.event
 async def on_message(message):
     channel_id = message.channel.id
     if channel_id == server_channel_id:
-        try:
-            attachment_url = message.attachments[0].url
-            os.system("tmux kill-session -t bin")
-            print("Killed session")
-            applying_filter()
-            with open("filter.json", "w+") as f:
-                print("Wrote filter")
-                try:
-                    r = requests.get(attachment_url).text
-                    f.write(r)
-                except Exception as e:
-                    print("error", e)
-                    return ""
-                applied_filter()
-            time.sleep(2)
-            os.system("tmux new-session -d -s bin './binmaster-v2-linux'")
-            print("Started session")
-            success()
-        except:
-            print("No attachment found")
+        if "/attachments/" in message.content:
+            results = re.findall("discordapp.com\/attachments\/([^\/]+)\/([^\/]+)\/.*", message.content)
+            if results:
+                print(results)
+                attachment_url = f"https://cdn.discordapp.com/attachments/{results[-1][0]}/{results[-1][1]}/filter.json"
+                os.system("tmux kill-session -t bin")
+                print("Killed session")
+                applying_filter()
+                with open("filter.json", "w+") as f:
+                    print("Wrote filter")
+                    try:
+                        r = requests.get(attachment_url).text
+                        f.write(r)
+                    except Exception as e:
+                        print("error", e)
+                        return ""
+                    applied_filter()
+        else:
+            try:
+                attachment_url = message.attachments[0].url
+                os.system("tmux kill-session -t bin")
+                print("Killed session")
+                applying_filter()
+                with open("filter.json", "w+") as f:
+                    print("Wrote filter")
+                    try:
+                        r = requests.get(attachment_url).text
+                        f.write(r)
+                    except Exception as e:
+                        print("error", e)
+                        return ""
+                    applied_filter()
+                print("Operation success")
+                time.sleep(2)
+                os.system("tmux new-session -d -s bin './binmaster-v2-linux'")
+                print("Started session")
+                success()
+            except:
+                print("No attachment found")
 
 
 client.run(token)
